79c5db14a4ccf624d2c319f82f7f9d52fbd45297
明晓 宋 <1587253748@qq.com>
2021-09-24 15:16:52 +0800

    1

--- Level_Changed.h
+++ Level_Changed.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Draw.h"
+#include "Pieces.h"
+#include <windows.h>
+
+class level{
+public:
+	level(Draw &da,int width=10,int heigh=20);
+	~level();
+
+	void drawboard()const;
+	bool rotating();
+	bool move(int xdistance,int ydistance);
+
+	bool update();
+	bool is_gameover();
+
+	void speed()const;
+	void score()const;
+};
